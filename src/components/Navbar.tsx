0a7e3f90ad8abd2c1c5b19571c719a20b85f1155
Angel Lakra <angel.lakra@chainlabs.in>
2023-07-22 01:12:50 +0530

    removed dark mode switch

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="w-screen px-[18px] py-2 flex justify-center items-center border dark:border-neutral-800 dark:bg-neutral-900">
+    <div className="w-screen px-[18px] py-2 flex justify-center items-center border border-neutral-800 bg-neutral-900">
       <div className="w-full md:w-[1440px] h-[34px] flex items-center">
         <div className="relative h-[15.7px] w-[79.4px] md:h-[25.66px] md:w-[126px]">
           <Image src={BRAND_DARK} alt="brand_logo" fill />
