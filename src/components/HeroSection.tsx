2820bc4b59705c4486cdb509f4291dfd6b05ca34
Angel Lakra <angel.lakra@chainlabs.in>
2023-07-24 15:17:50 +0530

    added interaction 2 animation

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -8,7 +8,7 @@ const HeroSection = (props: Props) => {
       <div className="w-[90%] md:w-[1440px] flex flex-col items-center">
         <div className="md:w-[560px] w-full mb-12 md:mb-[42px]">
           <h1 className="text-center md:text-5xl text-[32px] font-medium leading-10 md:leading-[57.6px] tracking-[-0.32px] md:tracking-[-0.96px]">
-            Peer-Peer lending protocol for ordinals
+            The premier platform for P2P ordinals lending
           </h1>
           <p className="text-center text-zinc-500 text-sm md:text-lg mt-2 md:mt-3 leading-5 md:leading-[21.6px] md:tracking-[-0.36px]">
             Instantly lend and borrow bitcoin against high-value ordinals
--- src/components/InteractiveSection/Interaction2.tsx
+++ src/components/InteractiveSection/Interaction2.tsx
@@ -1,16 +1,63 @@
 import { BITCOIN, BRAND_DARK } from "@/constants";
 import Image from "next/image";
-import { AnimationSequence, animate } from "framer-motion";
-import { useEffect } from "react";
+import { cubicBezier, easeIn, easeInOut, motion } from "framer-motion";
+import { useEffect, useState } from "react";
+
+const DIRECTION = {
+  LEFT: "LEFT",
+  RIGHT: "RIGHT",
+  HALT: "HALT",
+};
 
 const Interaction2 = () => {
+  const [animationDirection, setAnimationDirection] = useState(DIRECTION.RIGHT);
+  const [animationEnd, setAnimationEnd] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (animationDirection === DIRECTION.LEFT)
+        setAnimationDirection(DIRECTION.RIGHT);
+      if (animationDirection === DIRECTION.RIGHT)
+        setAnimationDirection(DIRECTION.LEFT);
+    }, 7000);
+  }, [animationDirection]);
+
   return (
     <div className="md:h-[192px] pt-[42px] md:pt-[46px] pb-[18px] md:pb-5 px-4 md:px-5 border border-neutral-800 rounded-[10px] flex flex-col justify-between bg-neutral-900">
       <div className="w-full flex items-center">
-        <div className="px-1.5 py-1 bg-[#1d1d1f] rounded-[7px]" id="icebox">
+        <motion.div
+          variants={{
+            shrink: { scale: [1, 0.9, 1] },
+            grow: {
+              scale: [1, 1.1, 1],
+            },
+          }}
+          animate={
+            animationDirection === DIRECTION.RIGHT
+              ? "shrink"
+              : animationDirection === DIRECTION.LEFT
+              ? "grow"
+              : ""
+          }
+          transition={
+            animationDirection === DIRECTION.RIGHT
+              ? {
+                  delay: 1.5,
+                  duration: 1,
+                }
+              : animationDirection === DIRECTION.LEFT
+              ? {
+                  delay: 3.5,
+                  duration: 1,
+                }
+              : {}
+          }
+          className="px-1.5 py-1.5 bg-[#1d1d1f] rounded-[7px] z-20"
+          id="icebox"
+        >
           <Image src={BRAND_DARK} alt="branding" width={49.2} height={10} />
-        </div>
-        <div className="h-full flex-1 overflow-hidden relative ">
+        </motion.div>
+        <div className="h-full flex-1 overflow-hidden relative mx-[-4px] ">
           <svg
             width="350"
             height="20"
@@ -19,7 +66,7 @@ const Interaction2 = () => {
             className="absolute top-0.5 left-0 z-0"
           >
             <line
-              stroke-dasharray="15, 10, 5, 10, 15"
+              stroke-dasharray="12"
               x1="0"
               y1="10"
               x2="350"
@@ -28,24 +75,74 @@ const Interaction2 = () => {
                 zIndex: -1,
                 strokeWidth: "1px",
                 stroke: "rgba(193, 199, 208, 0.40)",
-                strokeDashoffset: 82,
-                animation: "lineMove 2s linear infinite",
+                strokeDashoffset: 150,
+                animation:
+                  animationDirection === DIRECTION.RIGHT
+                    ? "lineMoveRight 4s  linear"
+                    : animationDirection === DIRECTION.LEFT
+                    ? "lineMoveLeft 4s linear"
+                    : "",
               }}
             ></line>
           </svg>
-          <div
-            className="bg-[#446148] py-[5px] px-2.5 rounded-[20px] text-[10px] font-medium leading-3 text-[#55f567] inline-block z-10 absolute top-0 left-0"
+          <motion.div
+            animate={
+              animationDirection === DIRECTION.RIGHT
+                ? "leftToRight"
+                : animationDirection === DIRECTION.LEFT
+                ? "rightToLeft"
+                : ""
+            }
+            transition={{
+              duration: 6,
+              ease: cubicBezier(0.0, 0.1, 1.0, 0.9),
+            }}
+            variants={{
+              leftToRight: {
+                x: [-600, -34, 122, 600],
+              },
+              rightToLeft: { x: [600, 122, -34, -600] },
+            }}
+            className="bg-[#446148] py-[5px] px-2.5 rounded-[20px] text-[10px] font-medium leading-3 text-[#55f567] z-10 absolute top-0 left-0"
+            style={{ opacity: animationDirection === DIRECTION.HALT ? 0 : 1 }}
             id="token"
           >
             Degods #2407
-          </div>
+          </motion.div>
         </div>
-        <div className="px-1.5 py-1 bg-[#1d1d1f] rounded-[7px] flex" id="btc">
+        <motion.div
+          variants={{
+            shrink: { scale: [1, 0.9, 1] },
+            grow: { scale: [1, 1.1, 1] },
+          }}
+          animate={
+            animationDirection === DIRECTION.RIGHT
+              ? "grow"
+              : animationDirection === DIRECTION.LEFT
+              ? "shrink"
+              : ""
+          }
+          transition={
+            animationDirection === DIRECTION.RIGHT
+              ? {
+                  delay: 3.5,
+                  duration: 1,
+                }
+              : animationDirection === DIRECTION.LEFT
+              ? {
+                  delay: 1,
+                  duration: 1,
+                }
+              : {}
+          }
+          className="px-1.5 py-1 bg-[#1d1d1f] rounded-[7px] flex z-10"
+          id="btc"
+        >
           <Image src={BITCOIN} alt="bitcoin" width={12} height={12} />
           <div className="ml-1 text-[#FCFCFC] text-xs font-semibold">
             Bitcoin
           </div>
-        </div>
+        </motion.div>
       </div>
       <div className="mt-[42px] md:mt-0">
         <div className="text-[#FCFCFC] text-sm font-semibold leading-[16.6px]">
